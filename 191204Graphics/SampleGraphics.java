464f58148dc4378eb028607ec0e7e2d38fe0589b
neonmarshmallow <suethan0011@gmail.com>
2019-12-09 14:57:59 -0800

    191209 Graphics progress, it's now a snow globe :)

--- 191204Graphics/SampleGraphics.java
+++ 191204Graphics/SampleGraphics.java
@@ -22,7 +22,7 @@ public class SampleGraphics
         Graphics g;
       
         win = new JFrame("My First Graphics");  // window setup
-        win.setSize(300,200);
+        win.setSize(800,600);
         win.setLocation(100,100);
         win.setVisible(true);
       
@@ -34,21 +34,47 @@ public class SampleGraphics
         //  before attempting to draw on it
         try {Thread.sleep(400);} catch (Exception e) {}
       
-        g.drawRect(50,50,100,30);   // draws outline rectangle
-      
-        g.setColor(Color.GREEN);
-       
-        g.fillOval(100,150,50,20);  // draws solid ovals and circles
-      
-        // set color to any RGB value
-        g.setColor(new Color(255,20,175));
-       
-        // draw a triangle
-        Polygon tri1 = new Polygon();
-        tri1.addPoint(200,100);
-        tri1.addPoint(250,150);
-        tri1.addPoint(180,180);
-        g.fillPolygon(tri1);       // drawPolygon() would create outline only
+        g.setColor(new Color(135,206,235)); // background color
 
+        g.fillRect(0,0,800,600);   // fills background
+
+        g.setColor(new Color(212,255,253)); // snowglobe color
+       
+        g.fillArc(150,260,500,500,0,180); // fills snowglobe (arc)
+
+        g.setColor(new Color(171,91,0)); // base color
+       
+        // draw a base of snowglobe (trapezoid)
+        Polygon base = new Polygon(); //top line corner 1 & 2, bottom line corner 3 & 4
+        base.addPoint(150,510); // corner 1
+        base.addPoint(125,600); // corner 3
+        base.addPoint(675,600); // corner 4
+        base.addPoint(650,510); //corner 2
+
+        g.fillPolygon(base); // fills base
+
+        g.setColor(Color.WHITE); // snowman color
+
+        g.fillOval(410,410,100,100); // bottom snowman ball
+        g.fillOval(425,370,70,70); // middle snowman ball
+        g.fillOval(440,340,40,40); // top snowman ball
+
+        g.setColor(Color.ORANGE); // carrot nose color
+
+        Polygon nose = new Polygon();
+        nose.addPoint(420,353);
+        nose.addPoint(445,360);
+        nose.addPoint(445,350);
+
+        g.fillPolygon(nose); // fills nose
+
+        g.setColor(Color.BLACK);
+
+        g.fillRect(450,310,20,30); // top hat
+        g.fillRect(440,330,40,10);
+
+        g.fillOval(450,350,7,7); // eye
+
+        g.drawArc(150,260,500,500,0,180); // snowglobe outline
    }
 }
