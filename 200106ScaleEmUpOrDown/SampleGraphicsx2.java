8f28ba460b432846ed58737798fdda7f5a1cfb08
neonmarshmallow <suethan0011@gmail.com>
2020-01-08 09:14:42 -0800

    200108 Scaler whitespace filter added

--- 200106ScaleEmUpOrDown/SampleGraphicsx2.java
+++ 200106ScaleEmUpOrDown/SampleGraphicsx2.java
@@ -1,18 +1,16 @@
 /*
  * Sample Graphics
  *
-
+ * Since we have a few days to fill till Christmas break, let's dip our toes
  * into graphics.  Blue Pelican has nothing, Head First...has too much, so use
  * the internet as your reference, and look up the commands below, or just
  * play with it to figure out what they do.
  *
-
+ * Then, draw a Christmas tree.  Or a snowflake?  Or something else holiday-/
  * vacation-/gonna-get-sleep-soon-related.
  */
-
 import javax.swing.*;  // for JFrame
 import java.awt.*;     // for Graphics and Container
-
 public class SampleGraphicsx2
 {  
     public static void main (String[] args)
@@ -22,7 +20,7 @@ public class SampleGraphicsx2
         Graphics g;
       
         win = new JFrame("My First Graphics");  // window setup
-        win.setSize(800,600);
+        win.setSize(1600,1200);
         win.setLocation(100,100);
         win.setVisible(true);
       
@@ -31,7 +29,7 @@ public class SampleGraphicsx2
       
         //  If you get a blank window, your computer's speed (lack thereof?)
         //  requires this delay in order to give the window time to appear
-
+        //  before attempting to draw on it
         try {Thread.sleep(400);} catch (Exception e) {}
       
         here:
@@ -39,112 +37,105 @@ public class SampleGraphicsx2
             for(int a = 0; a == 0;)
             {
                 g.setColor(new Color(135,206,235)); // background color
-
-
+                g.fillRect(0,0,1600,1200);   // fills background
         
                 g.setColor(new Color(212,255,253)); // snowglobe color
                
-
+                g.fillArc(300,520,1000,1000,0,360); // fills snowglobe (arc)
         
                 g.setColor(new Color(171,91,0)); // base color
                
-
+                // draw a base of snowglobe (trapezoid)
                 Polygon base = new Polygon(); //top line corner 1 & 2, bottom line corner 3 & 4
-
-
-
-
+                base.addPoint(300,1020); // corner 2
+                base.addPoint(250,1200); // corner 6
+                base.addPoint(1350,1200); // corner 8
+                base.addPoint(1300,1020); //corner 4
         
-
+                g.fillPolygon(base); // fills base
         
                 g.setColor(Color.WHITE); // snowman color
         
-
-
-
+                g.fillOval(820,820,200,200); // bottom snowman ball
+                g.fillOval(850,740,140,140); // middle snowman ball
+                g.fillOval(880,680,80,80); // top snowman ball
         
                 g.setColor(Color.ORANGE); // carrot nose color
         
                 Polygon nose = new Polygon();
-
-
-
+                nose.addPoint(840,706);
+                nose.addPoint(890,720);
+                nose.addPoint(890,700);
         
-
+                g.fillPolygon(nose); // fills nose
         
                 g.setColor(Color.BLACK);
         
-
-
+                g.fillRect(900,620,40,60); // top hat
+                g.fillRect(880,670,80,20);
         
-
+                g.fillOval(900,700,14,14); // eye
         
-
-
-
+                g.fillOval(910,760,14,14); // button top
+                g.fillOval(910,780,14,14); // button middle
+                g.fillOval(910,800,14,14); // button bottom
         
-
+                g.drawArc(300,520,1000,1000,0,360); // snowglobe outline
         
                 Polygon oBase = new Polygon();
-
-
-
-
+                oBase.addPoint(300,1020); // corner 2
+                oBase.addPoint(250,1200); // corner 6
+                oBase.addPoint(1350,1200); // corner 8
+                oBase.addPoint(1300,1020); //corner 4
         
-
+                g.drawPolygon(oBase); // outlines base
         
                 g.setColor(Color.GREEN);
         
-
+                g.fillRect(540,800,200,218); // present box
         
                 g.setColor(Color.RED);
         
-
+                g.fillRect(620,800,40,218); // present ribbon
         
                 Polygon bow = new Polygon();
-
-
-
-
-
-
-
-
+                bow.addPoint(610,770);
+                bow.addPoint(600,780);
+                bow.addPoint(610,790);
+                bow.addPoint(640,800);
+                bow.addPoint(670,790);
+                bow.addPoint(680,780);
+                bow.addPoint(670,770);
+                bow.addPoint(640,790);
         
-
+                g.fillPolygon(bow); // fills bow
         
                 // snow section
                 g.setColor(Color.WHITE);
         
                 int reset = 0;
-
                 // snow loop
                 for(int i = 0; i < 801; i++)
                 {
                     for(int j = 0; j < 601; j++)
                     {
                         try {Thread.sleep(100);} catch (Exception e) {}
-
                         int x = (int)(800 * Math.random());
                         int y = (int)(600 * Math.random());
-
                         int flake1 = 1;
                         int flip1 = (int)(10 * Math.random()); // coin flip for x offset
                         if (flip1 < 5)
                         {
                             flake1 = -1;
                         }
-
                         int flake2 = 1;
                         int flip2 = (int)(10 * Math.random()); // coin flip for y offset
                         if (flip2 < 5)
                         {
                             flake2 = -1;
                         }
-
-
-
-
+                        g.fillOval(x, y, 6, 6);
+                        g.drawOval(x + flake1, y + flake2, 8, 8); // ensures every snow flake is "unique"
                         reset++;
                         if(reset == 500) // resets at 500 snowflakes
                         {
--- 200106ScaleEmUpOrDown/Scaler.java
+++ 200106ScaleEmUpOrDown/Scaler.java
@@ -7,9 +7,9 @@ public class Scaler
         File myfile = new File(args[0] + ".java");
         Scanner filereader = new Scanner(myfile);
 
-        java.util.Scanner kbin = new java.util.Scanner(System.in);
+        Scanner kbin = new Scanner(System.in);
         System.out.print("Scale by what percent: ");
-        double magnitude = kbin.nextInt() / 100;
+        double magnitude = kbin.nextDouble() / 100;
 
         FileWriter fw = new FileWriter(args[0] + "x" + (int)magnitude + ".java"); //change # to have magnitude altered
         PrintWriter output = new PrintWriter(fw);
@@ -18,24 +18,31 @@ public class Scaler
         {
             String line = filereader.nextLine();
 
-            if(line.matches(args[0]))
+            if(line.matches("(.*)" + args[0]))
             {
                 output.println("public class " + args[0] + "x" + (int)magnitude);
+                continue;
             }
-            
-            if(line.matches("(.*)fill(.*)") || line.matches("(.*)add(.*)") || line.matches("(.*)draw(.*)") || line.matches("(.*)size(.*)"))
+            if(line.matches("(.*)\\.fill(.*)") || line.matches("(.*)\\.add(.*)") || line.matches("(.*)\\.draw(.*)") || line.matches("(.*)\\.setSize(.*)"))
             {
                 output.println(rescaler(line, magnitude));
+                continue;
+            }
+            if(line.equals(""))
+            {
+                continue;
             }
             else
             {
                 output.println(line);
+                continue;
             }
         }
 
         output.close();
         fw.close();
         filereader.close();
+        kbin.close();
     }
 
     public static String rescaler(String a, double b)
@@ -44,6 +51,11 @@ public class Scaler
 
         String[] word = a.split("\\d+");
 
+        for(int i = 0; i < word.length; i++) // word test
+        {
+            System.out.println("word: " + word[i]);
+        }
+
         int[] numI = new int[numS.length];
 
         for(int i = 0; i < numS.length; i++)
@@ -51,6 +63,7 @@ public class Scaler
             if(!numS[i].equals(""))
             {
                 numI[i] =(int) (Integer.parseInt(numS[i]) * b); //change the two to a magnitude chosen
+                System.out.println("int: " + numS[i]); // number test
             }
         }
 
@@ -62,7 +75,7 @@ public class Scaler
             sleep = numI.length;
         }
 
-        if(((int)a.charAt(0)) >= 65 && ((int)a.charAt(0)) <= 90 || ((int)a.charAt(0)) >= 97 && ((int)a.charAt(0)) <= 122)
+        if(((int)a.charAt(0)) >= 65 && ((int)a.charAt(0)) <= 90 || ((int)a.charAt(0)) >= 97 && ((int)a.charAt(0)) <= 122 || ((int)a.charAt(0) == 32))
         {
             for(int i = 0; i < sleep; i++)
             {
@@ -93,7 +106,6 @@ public class Scaler
                 }
             }
         }
-
         return whole;
     }
 }
