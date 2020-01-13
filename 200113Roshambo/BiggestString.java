d3b7fffdebbb7bfe3a952491f3597c76630071e7
neonmarshmallow <suethan0011@gmail.com>
2020-01-13 14:14:16 -0800

    200113 Roshambo has begun

---
+++ 200113Roshambo/BiggestString.java
@@ -0,0 +1,44 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class BiggestString
+{
+	private String one,two,three;
+
+	public BiggestString()
+	{
+		this("","","");
+	}
+
+	public BiggestString(String a, String b, String c)
+	{
+
+
+	}
+
+	public void setStrings(String a, String b, String c)
+	{
+
+
+
+	}
+
+	public String getBiggest()
+	{
+
+
+
+
+		return "";
+	}
+
+	public String toString()
+	{
+	   return "";
+	}
+}
---
+++ 200113Roshambo/RockPaperScissors.java
@@ -0,0 +1,79 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class RockPaperScissors
+{
+	private String playChoice;
+	private String compChoice;
+
+	public RockPaperScissors()
+	{
+
+
+
+	}
+
+	public RockPaperScissors(String player)
+	{
+
+
+
+
+
+
+
+	}
+
+	public void setPlayers(String player)
+	{
+
+
+
+
+
+
+
+	}
+
+	public String determineWinner()
+	{
+		String winner="";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		return winner;
+	}
+
+	public String toString()
+	{
+		String output="";
+
+
+
+
+		return output;
+	}
+}
