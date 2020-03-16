e8d2d90d94b867cc34717fb8bc7d8d56d8c1d433
neonmarshmallow <suethan0011@gmail.com>
2020-03-15 21:04:34 -0700

    200313 MultiKeySort progress

--- 200311MultiKeySort/MultiKeySort.java
+++ 200311MultiKeySort/MultiKeySort.java
@@ -1,8 +1,21 @@
+import java.io.IOException;
+
 public class MultiKeySort
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         File myfile = new File("Names_ages.txt");
-        Scanner
+        Scanner filereader = new Scanner(myfile);
+
+        String[] list = new String[1000];
+        int maxIndex = -1;
+
+        while(filereader.hasNext())
+        {
+            maxIndex++;
+            text[maxIndex] = filereader.nextLine();
+            System.out.println(text[maxIndex]);
+        }
+        filereader.close();
     }
 }
