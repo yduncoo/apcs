fe87c3a9c44444e1ce881a9ca6de10ff2a40b3ab
Ethan Su <suethan0011@gmail.com>
2020-04-30 18:19:34 -0700

    200430 AP Practice Test demo

---
+++ 200430APPracticeTest/question1.java
@@ -0,0 +1,9 @@
+
+public class question1 {
+    public static void main(String[] args)
+    {
+        int tester;
+
+        Scanner kbin;
+    }
+}
---
+++ 200430APPracticeTest/question2.java
@@ -0,0 +1,20 @@
+
+public class question2 {
+    private String name;
+    public question2(String input)
+    {
+        name = input;
+    }
+
+    public String sentence()
+    {
+        return "hello there " + name;
+    }
+
+    // goo goo ga ga im baby
+    // el mega chungus
+    // ojisan to marshmallow
+    // o  p  o
+    /* it was at that moment that he realized that there wasn't really an answer to it all in the end
+    so he ended up giving it all to */
+}
