f046795683fafdde91ff6c38248956364a1094f0
Ethan Su <suethan0011@gmail.com>
2020-05-18 21:13:25 -0700

    200518 hehe xd progress on nong and git sync

---
+++ 200512nong/mapping.java
@@ -0,0 +1,11 @@
+public class mapping
+{
+    private Object[][] oMap; //object map
+    private Object[][] tmap; //terrain map
+
+    public mapping()
+    {
+        oMap = new Object[10][16];
+        tMap = new Object[10][16];
+    }
+}
